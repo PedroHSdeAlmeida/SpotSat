f4b858bd6376696a4dc5e2d1858a8647d3e798ad
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2023-05-27 21:26:35 -0300

    Concluída segunda task da segunda parte: Retorna um lugar da lista JSON de acordo com o id enviado
    Foram realizadas as seguintes alterações:
    
    Refatorando 'getPlaces.ts' adicionando uma rota que busca por id
    Refatorando 'places.test.ts' para verificar se a nova rota está funcionando corretamente

--- src/routes/getPlaces.ts
+++ src/routes/getPlaces.ts
@@ -1,24 +1,35 @@
 import express, { Request, Response } from 'express';
 
 const placeRouter = express.Router();
-  
-  placeRouter.get('/', (req: Request, res: Response) => {
-    const places = [
-        {
-          id: 1,
-          name: 'Parque da Cidade',
-          latitude: -23.221112,
-          longitude: -45.899678,
-        },
-        {
-          id: 2,
-          name: 'Praça Ulisses Guimarães',
-          latitude: -23.180038,
-          longitude: -45.884357,
-        },
-      ];
-    res.json([places]);
 
-  });
-  
+const places = [
+  {
+    id: 1,
+    name: 'Parque da Cidade',
+    latitude: -23.221112,
+    longitude: -45.899678,
+  },
+  {
+    id: 2,
+    name: 'Praça Ulisses Guimarães',
+    latitude: -23.180038,
+    longitude: -45.884357,
+  },
+];
+
+placeRouter.get('/', (req: Request, res: Response) => {
+  res.json(places);
+});
+
+placeRouter.get('/:id', (req: Request, res: Response) => { 
+  const { id } = req.params;
+  const place = places.find((p) => p.id === Number(id));
+
+  if (!place) {
+    res.status(404).json({ error: 'Lugar não encontrado' });
+  } else {
+    res.json(place);
+  }
+});
+
 export default placeRouter;
--- tests/places.test.ts
+++ tests/places.test.ts
@@ -1,13 +1,32 @@
 import request from 'supertest';
 import app from '../src/app';
 
-
 describe('GET /v2/places', () => {
-    test('Deve retornar uma lista de lugares', async () => {
-      const response = await request(app).get('/v2/places');
-  
-      expect(response.status).toBe(200);
-      expect(response.body[0]).toHaveLength(2); 
+  test('Deve retornar uma lista de lugares', async () => {
+    const response = await request(app).get('/v2/places');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(2);
+  });
+});
+
+describe('GET /v2/places/:id', () => {
+  test('Deve retornar um lugar específico pelo ID', async () => {
+    const response = await request(app).get('/v2/places/1');
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: 1,
+      name: 'Parque da Cidade',
+      latitude: -23.221112,
+      longitude: -45.899678,
     });
   });
-  
+
+  test('Deve retornar 404 se o lugar não for encontrado', async () => {
+    const response = await request(app).get('/v2/places/999');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ error: 'Lugar não encontrado' });
+  });
+});
